e6da47c0722b19f704521dcc5eba9ba3615cb0dd
Tobias Thygesen <tth@nine.dk>
2024-02-14 21:34:17 +0100

    Flyt alt ind i [locale]

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,10 @@
+import MyComponent from "@/components/MyComponent";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <MyComponent/>
+    </main>
+  );
+}
