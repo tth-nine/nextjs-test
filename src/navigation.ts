b43cf52fea8a39a536001c7d841e12790f7df507
Tobias Thygesen <tth@nine.dk>
2024-02-14 22:07:38 +0100

    Tilføj navigation

---
+++ src/navigation.ts
@@ -0,0 +1,6 @@
+import {createSharedPathnamesNavigation} from 'next-intl/navigation';
+ 
+export const locales = ['da', 'en'] as const;
+export const localePrefix = 'as-needed';
+ 
+export const {Link, redirect, usePathname, useRouter} = createSharedPathnamesNavigation({locales, localePrefix});
